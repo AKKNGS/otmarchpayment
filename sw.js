e9caa0d62a8f9e8c0b9fe203177e8efaed07cd9e
sandada035-blip <sandada035@gmail.com>
2026-03-02 10:11:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'budget-app-v1';
+const CACHE_NAME = 'budget-app-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -23,4 +23,5 @@ self.addEventListener('fetch', event => {
       return response || fetch(event.request);
     })
   );
+
 });
